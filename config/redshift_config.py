c92c4cd2ea1edc0e2e5baea79a7328f8a0334f35
FRANCIS XAVIER inyangat <xavierinyangat@franciss-mbp.3500.dhcp.asu.edu>
2025-03-29 17:27:12 -0700

    cachced

--- config/redshift_config.py
+++ config/redshift_config.py
@@ -1,11 +1,11 @@
 REDSHIFT_CONFIG = {
-    "host": "retail-etl-cluster.cqy7kya7yxbw.us-east-1.redshift.amazonaws.com",
+    "host": "",
     "port": 5439,
-    "user": "admin",
-    "password": "Admin12345678",
+    "user": "",
+    "password": "",
     "database": "dev",
     "table": "sales",
-    "iam_role": "arn:aws:iam::390403886320:role/service-role/AmazonRedshift-CommandsAccessRole-20250329T151542",
+    "iam_role": "",
     "s3_bucket":"xavier-etl-bucket",
     "s3_key": "etl/sales/daily_sales_2025-03-29.csv"
 
