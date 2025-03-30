ef6c4ef488740a6511e805f0657cb295fc3f2e28
FRANCIS XAVIER inyangat <xavierinyangat@franciss-mbp.3500.dhcp.asu.edu>
2025-03-29 17:26:15 -0700

    ignore chace

--- config/db_config.py
+++ config/db_config.py
@@ -1,3 +1,3 @@
 # DB and S3 Credentials
-DB_URI = ""
+DB_URI = "mysql+pymysql://eda2025:msba@cis505-shi.cloud.wpcarey.asu.edu:3306/Northwind"
 
--- config/redshift_config.py
+++ config/redshift_config.py
@@ -1,11 +1,11 @@
 REDSHIFT_CONFIG = {
-    "host": "",
+    "host": "retail-etl-cluster.cqy7kya7yxbw.us-east-1.redshift.amazonaws.com",
     "port": 5439,
-    "user": "",
-    "password": "",
-    "database": "",
+    "user": "admin",
+    "password": "Admin12345678",
+    "database": "dev",
     "table": "sales",
-    "iam_role": "",
+    "iam_role": "arn:aws:iam::390403886320:role/service-role/AmazonRedshift-CommandsAccessRole-20250329T151542",
     "s3_bucket":"xavier-etl-bucket",
     "s3_key": "etl/sales/daily_sales_2025-03-29.csv"
 
